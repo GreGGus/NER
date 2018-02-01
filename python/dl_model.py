7073bff79cc2a0d52a9475ad29e6dd6503bbd721
GreGGus <gportier@ece.fr>
2018-02-01 23:29:49 +0100

    init

---
+++ python/dl_model.py
@@ -0,0 +1,21 @@
+import os
+
+import anago
+from anago.utils import download
+from anago.reader import load_data_and_labels
+
+dir_path = 'test_dir'
+url = 'https://storage.googleapis.com/chakki/datasets/public/models.zip'
+DATA_ROOT = os.path.join(os.path.dirname(__file__), '/home/gregoire.portier/NER/"')
+
+test_path = os.path.join(DATA_ROOT, 'test.txt')
+x_test, y_test = load_data_and_labels(test_path)
+print("loead")
+
+print("start dl")
+download(url, dir_path)
+print("end dl")
+
+model = anago.Sequence.load(dir_path)
+print("model load")
+model.eval(x_test, y_test)
