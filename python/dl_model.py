96810bc0cd6400fb148e527a5780b762778c513b
GreGGus <gportier@ece.fr>
2018-02-01 23:36:34 +0100

    - add test path

--- python/dl_model.py
+++ python/dl_model.py
@@ -6,7 +6,7 @@ from anago.reader import load_data_and_labels
 
 dir_path = 'test_dir'
 url = 'https://storage.googleapis.com/chakki/datasets/public/models.zip'
-DATA_ROOT = os.path.join(os.path.dirname(__file__), '/home/gregoire.portier/NER/"')
+DATA_ROOT = os.path.join(os.path.dirname(__file__), '/home/gregoire.portier/NER/test.txt')
 
 test_path = os.path.join(DATA_ROOT, 'test.txt')
 x_test, y_test = load_data_and_labels(test_path)
