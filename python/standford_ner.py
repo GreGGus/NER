020ffbb6f0afc5c5381397c31ac86431a1eb4b40
GreGGus <gportier@ece.fr>
2018-02-02 12:29:33 +0100

    -fix import tagger

--- python/standford_ner.py
+++ python/standford_ner.py
@@ -1,11 +1,11 @@
 import os
 
-from nltk.tag.stanford import NERTagger
+from nltk.tag.stanford import StanfordNERTagger
 
 
 classifier_path='/home/gregoire.portier/NER/NER/python/classifier/all.3class.distsim.crf.ser.gz'
 jar_path='/home/gregoire.portier/NER/NER/python/classifier/stanford-ner.jar'
-st = NERTagger(classifier_path,jar_path)
+st = StanfordNERTagger(classifier_path,jar_path)
 
 st.tag('Rami Eid is studying at Stony Brook University in NY'.split())
 
